bad2eeeddc932df7e27ad9042ab26667f6676623
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2018-12-09 12:40:13 +0100

    v0.1a first helper method added

--- src/Main/Nonogram.java
+++ src/Main/Nonogram.java
@@ -2,15 +2,53 @@ package Main;
 
 import Draw.Draw_Main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static Helpers.StringHelpers.Strings.*;
+
 /**
  * @author Andreas Ambühl
- * @version 0.1
+ * @version 0.1a
  */
 public class Nonogram {
     public static void main(String[] args) {
-        Draw_Main.setWidth(500);
+        // some initial value (in case the scanner would fail)
+        int width = 5;
+        int height = 5;
+        int boxSize = 5;
+        String fileName = "Examples/nonogram1.txt";
+
+
+        List<String> input = new ArrayList<>();
+        File file = new File("src/" + fileName);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                input.add(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println("Input so far:");
+//        input.forEach(System.out::println);
+
+        //
+        for (String s : input) {
+            if (s.toLowerCase().contains("width")) {
+                width = getLastIntegerFromString(s);
+            }
+
+        }
+
+        Draw_Main.setWidth(width);
         Draw_Main.setHeight(500);
         Draw_Main.setBoxSize(10);
-        Draw_Main.main(args);
+//        Draw_Main.main(args);
     }
+
 }
